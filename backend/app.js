cadee07bc5786b151416b2a1c5b67ef08d2db158
ParadiseSeeker <paradiseseeker94@gmail.com>
2020-04-29 18:49:18 +0300

    :/

--- backend/app.js
+++ backend/app.js
@@ -18,16 +18,12 @@ app.use("/api/pc", require("./routes/pc.routes"))
 app.use("/api/components", require("./routes/components.routes"))
 app.use("/api/manufacturers", require("./routes/manufacturers.routes"))
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use("/", express.static(path.join(__dirname, "frontend", "build")))
-
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-// 	})
-// }
-
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("frontend/build"))
+	app.use("/", express.static(path.join(__dirname, "frontend", "build")))
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+	})
 }
 
 async function start() {
